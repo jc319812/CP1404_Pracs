bbde4b92f30b7f0fff6d6297eb2e8125251c40c9
jc319812 <abdalnasser.zamara@my.jcu.edu.au>
2017-09-14 00:21:29 +1000

    caps - prac 5

---
+++ caps.py
@@ -0,0 +1,16 @@
+
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA":"South Australia"}
+
+state = input("Enter short state: ").upper()
+while state != "":
+    if state in STATE_NAMES:
+        print(state, "is", STATE_NAMES[state])
+    else:
+        print("Invalid short state")
+    state = input("Enter short state: ").upper()
+
+
+for key in STATE_NAMES:
+    print("{:3} is {:3}".format(key, STATE_NAMES[key]))
